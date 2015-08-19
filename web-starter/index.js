4d7a9ad7b21bcc3ad31490cb974983b8da2ef9b8
William Hurley <whurley@forumone.com>
2015-08-19 09:45:02 -0400

    Initial commit

---
+++ web-starter/index.js
@@ -0,0 +1,127 @@
+'use strict';
+var generators = require('yeoman-generator'), 
+  _ = require('lodash');
+
+module.exports = generators.Base.extend({
+  engine : require('yeoman-hoganjs-engine'),
+  prompting : function() {
+    var done = this.async();
+    var config = _.extend({
+      host : 'f1dev',
+      vhost : '/vagrant/public',
+      database : true,
+      db_name : 'web',
+      db_user : 'web',
+      php_version : 'php55u',
+      solr : true
+    }, this.config.getAll());
+
+    this.prompt([{
+      type : 'list',
+      name : 'host',
+      message : 'Which host will it be deployed to?',
+      default : config.host,
+      choices : [{
+        name : 'F1',
+        value : 'f1dev',
+      },
+      {
+        name : 'Acquia',
+        value : 'acquia',
+      },
+      {
+        name : 'Pantheon',
+        value : 'pantheon',
+      }]
+    },
+    {
+      type : 'input',
+      name : 'vhost',
+      message : 'What is the path to the virtual host?',
+      default : config.vhost,
+    },
+    {
+      type : 'confirm',
+      name : 'database',
+      message : 'Does this project need a database?',
+      default : config.database,
+    },
+    {
+      type : 'input',
+      name : 'db_name',
+      message : 'What is the name of the database?',
+      default : config.db_name,
+      when : function(answers) {
+        return answers.database;
+      }
+    },
+    {
+      type : 'input',
+      name : 'db_user',
+      message : 'What is the user connecting to the database?',
+      default : config.db_user,
+      when : function(answers) {
+        return answers.database;
+      }
+    },
+    {
+      type : 'list',
+      name : 'php_version',
+      message : 'Which version of PHP to use?',
+      default : config.php_version,
+      choices : [{
+        name : 'None',
+        value : ''
+      },
+      {
+        name : '5.3',
+        value : 'php53'
+      },
+      {
+        name : '5.4',
+        value : 'php54u'
+      },
+      {
+        name : '5.5',
+        value : 'php55u'
+      }]
+    },
+    {
+      type: 'confirm',
+      name: 'solr',
+      message: 'Does it use Solr?',
+      default: config.solr,
+    }], function (answers) {
+      this.config.set(answers);
+
+      // Expose the answers on the parent generator
+      _.extend(this.options.parent.answers, { 'web-starter-puppet' : answers });
+
+      done();
+    }.bind(this));
+  },
+  writing : {
+    // Write Puppet manifest file
+    init : function() {
+      var done = this.async();
+      
+      var config = this.config.getAll();
+      _.extend(config, this.options.parent.answers);
+      
+      this.template('puppet/manifests/init.pp', 'puppet/manifests/init.pp', config);
+      
+      done();
+    },
+    // Write Hiera site file
+    site : function() {
+      var done = this.async();
+      
+      var config = this.config.getAll();
+      _.extend(config, this.options.parent.answers);
+      
+      this.template('puppet/manifests/hieradata/sites/localhost.localdomain.yaml', 'puppet/manifests/hieradata/sites/localhost.localdomain.yaml', config);
+      
+      done();
+    }
+  }
+});
