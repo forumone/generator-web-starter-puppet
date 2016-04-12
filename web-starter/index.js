fd3c34b3c1bcadc3af0b5d260eebf8252926db5d
William Hurley <whurley@forumone.com>
2016-04-12 17:26:27 -0400

    Updating to use Promises for prompts

--- web-starter/index.js
+++ web-starter/index.js
@@ -1,10 +1,12 @@
 'use strict';
 var generators = require('yeoman-generator'), 
-  _ = require('lodash');
+  _ = require('lodash')
+  Promise = require('bluebird');
 
 module.exports = generators.Base.extend({
   prompting : function() {
     var done = this.async();
+    var that = this;
     var config = _.extend({
       host : 'f1dev',
       vhost : '/vagrant/public',
@@ -15,93 +17,98 @@ module.exports = generators.Base.extend({
       solr : true
     }, this.config.getAll());
 
-    this.prompt([{
-      type : 'list',
-      name : 'host',
-      message : 'Which host will it be deployed to?',
-      default : config.host,
-      choices : [{
-        name : 'F1',
-        value : 'f1dev',
+    return new Promise(function(resolve, reject) {
+      that.prompt([{
+        type : 'list',
+        name : 'host',
+        message : 'Which host will it be deployed to?',
+        default : config.host,
+        choices : [{
+          name : 'F1',
+          value : 'f1dev',
+        },
+        {
+          name : 'Acquia',
+          value : 'acquia',
+        },
+        {
+          name : 'Pantheon',
+          value : 'pantheon',
+        }]
       },
       {
-        name : 'Acquia',
-        value : 'acquia',
+        type : 'input',
+        name : 'vhost',
+        message : 'What is the path to the virtual host?',
+        default : config.vhost,
       },
       {
-        name : 'Pantheon',
-        value : 'pantheon',
-      }]
-    },
-    {
-      type : 'input',
-      name : 'vhost',
-      message : 'What is the path to the virtual host?',
-      default : config.vhost,
-    },
-    {
-      type : 'confirm',
-      name : 'database',
-      message : 'Does this project need a database?',
-      default : config.database,
-    },
-    {
-      type : 'input',
-      name : 'db_name',
-      message : 'What is the name of the database?',
-      default : config.db_name,
-      when : function(answers) {
-        return answers.database;
-      }
-    },
-    {
-      type : 'input',
-      name : 'db_user',
-      message : 'What is the user connecting to the database?',
-      default : config.db_user,
-      when : function(answers) {
-        return answers.database;
-      }
-    },
-    {
-      type : 'list',
-      name : 'php_version',
-      message : 'Which version of PHP to use?',
-      default : config.php_version,
-      choices : [{
-        name : 'None',
-        value : ''
+        type : 'confirm',
+        name : 'database',
+        message : 'Does this project need a database?',
+        default : config.database,
       },
       {
-        name : '5.3',
-        value : 'php53'
+        type : 'input',
+        name : 'db_name',
+        message : 'What is the name of the database?',
+        default : config.db_name,
+        when : function(answers) {
+          return answers.database;
+        }
       },
       {
-        name : '5.4',
-        value : 'php54u'
+        type : 'input',
+        name : 'db_user',
+        message : 'What is the user connecting to the database?',
+        default : config.db_user,
+        when : function(answers) {
+          return answers.database;
+        }
       },
       {
-        name : '5.5',
-        value : 'php55u'
+        type : 'list',
+        name : 'php_version',
+        message : 'Which version of PHP to use?',
+        default : config.php_version,
+        choices : [{
+          name : 'None',
+          value : ''
+        },
+        {
+          name : '5.3',
+          value : 'php53'
+        },
+        {
+          name : '5.4',
+          value : 'php54u'
+        },
+        {
+          name : '5.5',
+          value : 'php55u'
+        },
+        {
+          name : '5.6',
+          value : 'php56u'
+        }]
       },
       {
-        name : '5.6',
-        value : 'php56u'
-      }]
-    },
-    {
-      type: 'confirm',
-      name: 'solr',
-      message: 'Does it use Solr?',
-      default: config.solr,
-    }], function (answers) {
-      this.config.set(answers);
-
+        type: 'confirm',
+        name: 'solr',
+        message: 'Does it use Solr?',
+        default: config.solr,
+      }], function(answers) {
+          resolve(answers);
+      });
+    }).then(function(answers) {
+      that.config.set(answers);
+        
+      answers.config = {};
       // Expose the answers on the parent generator
-      _.extend(this.options.parent.answers, { 'web-starter-puppet' : answers });
-
+      _.extend(that.options.parent.answers, { 'web-starter-puppet' : answers });
+    }).finally(function() {
       done();
-    }.bind(this));
+    });
   },
   writing : {
     // Write Puppet manifest file
