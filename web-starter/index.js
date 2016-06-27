f085ad4b5e196f71f5631cd670a32df7f9dd096c
William Hurley <whurley@forumone.com>
2016-06-27 17:07:49 -0400

    Removing old promise mechanism

--- web-starter/index.js
+++ web-starter/index.js
@@ -22,90 +22,88 @@ module.exports = generators.Base.extend({
       solr : true
     }, this.config.getAll());
 
-    return new Promise(function(resolve, reject) {
-      that.prompt([{
-        type : 'list',
-        name : 'host',
-        message : 'Which host will it be deployed to?',
-        default : config.host,
-        choices : [{
-          name : 'F1',
-          value : 'f1dev',
-        },
-        {
-          name : 'Acquia',
-          value : 'acquia',
-        },
-        {
-          name : 'Pantheon',
-          value : 'pantheon',
-        }]
+    return this.prompt([
+    {
+      type : 'list',
+      name : 'host',
+      message : 'Which host will it be deployed to?',
+      default : config.host,
+      choices : [{
+        name : 'F1',
+        value : 'f1dev',
       },
       {
-        type : 'input',
-        name : 'vhost',
-        message : 'What is the path to the virtual host?',
-        default : config.vhost,
+        name : 'Acquia',
+        value : 'acquia',
       },
       {
-        type : 'confirm',
-        name : 'database',
-        message : 'Does this project need a database?',
-        default : config.database,
+        name : 'Pantheon',
+        value : 'pantheon',
+      }]
+    },
+    {
+      type : 'input',
+      name : 'vhost',
+      message : 'What is the path to the virtual host?',
+      default : config.vhost,
+    },
+    {
+      type : 'confirm',
+      name : 'database',
+      message : 'Does this project need a database?',
+      default : config.database,
+    },
+    {
+      type : 'input',
+      name : 'db_name',
+      message : 'What is the name of the database?',
+      default : config.db_name,
+      when : function(answers) {
+        return answers.database;
+      }
+    },
+    {
+      type : 'input',
+      name : 'db_user',
+      message : 'What is the user connecting to the database?',
+      default : config.db_user,
+      when : function(answers) {
+        return answers.database;
+      }
+    },
+    {
+      type : 'list',
+      name : 'php_version',
+      message : 'Which version of PHP to use?',
+      default : config.php_version,
+      choices : [{
+        name : 'None',
+        value : ''
       },
       {
-        type : 'input',
-        name : 'db_name',
-        message : 'What is the name of the database?',
-        default : config.db_name,
-        when : function(answers) {
-          return answers.database;
-        }
+        name : '5.3',
+        value : 'php'
       },
       {
-        type : 'input',
-        name : 'db_user',
-        message : 'What is the user connecting to the database?',
-        default : config.db_user,
-        when : function(answers) {
-          return answers.database;
-        }
+        name : '5.4',
+        value : 'php54u'
       },
       {
-        type : 'list',
-        name : 'php_version',
-        message : 'Which version of PHP to use?',
-        default : config.php_version,
-        choices : [{
-          name : 'None',
-          value : ''
-        },
-        {
-          name : '5.3',
-          value : 'php'
-        },
-        {
-          name : '5.4',
-          value : 'php54u'
-        },
-        {
-          name : '5.5',
-          value : 'php55u'
-        },
-        {
-          name : '5.6',
-          value : 'php56u'
-        }]
+        name : '5.5',
+        value : 'php55u'
       },
       {
-        type: 'confirm',
-        name: 'solr',
-        message: 'Does it use Solr?',
-        default: config.solr,
-      }], function(answers) {
-          resolve(answers);
-      });
-    }).then(function(answers) {
+        name : '5.6',
+        value : 'php56u'
+      }]
+    },
+    {
+      type: 'confirm',
+      name: 'solr',
+      message: 'Does it use Solr?',
+      default: config.solr,
+    }])
+    .then(function(answers) {
       that.config.set(answers);
 
       answers.config = {};
@@ -116,8 +114,6 @@ module.exports = generators.Base.extend({
   writing : {
     // Write Puppet manifest file
     init : function() {
-      var done = this.async();
-
       // Get current system config for this sub-generator
       var config = this.options.parent.answers['web-starter-puppet'];
 
@@ -161,13 +157,9 @@ module.exports = generators.Base.extend({
         this.destinationPath('puppet/manifests/init.pp'),
         config
       );
-
-      done();
     },
     // Write Hiera site file
     site : function() {
-      var done = this.async();
-
       // Get current system config for this sub-generator
       var config = this.options.parent.answers['web-starter-puppet'];
       _.extend(config, this.options.parent.answers);
@@ -177,8 +169,6 @@ module.exports = generators.Base.extend({
         this.destinationPath('puppet/manifests/hieradata/sites/localhost.yaml'),
         config
       );
-
-      done();
     }
   }
 });
